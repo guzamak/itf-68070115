5b8c911a1314dfa86633464aef22cb93b45f850a
Pronwattana Wattanathamrong <noipoke164@gmail.com>
2025-10-03 11:53:56 +0700

    fdfds

---
+++ index.js
@@ -0,0 +1,9 @@
+const myinput = document.getElementById("myinput")
+
+const add = () => {
+   myinput.value = Number(myinput.value) + 1 
+}
+
+const minus = () => {
+   myinput.value = Number(myinput.value) - 1 
+}
